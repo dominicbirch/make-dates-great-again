dafdf1ff71568b7ff5d8c4da104f70dfbe208d7c
Dominic Birch <BDOMINI@mapfre.net>
2021-10-15 19:04:25 +0100

    WIP - updated dependencies

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,57 +1,70 @@
-import * as webpack from "webpack";
-import { resolve, join } from "path";
-import { CleanWebpackPlugin } from "clean-webpack-plugin";
-import { CombineDeclarationsWebpackPlugin } from "bundle-declarations-webpack-plugin";
-const
-    { name: packageName } = require("./package.json"),
-    outDir = resolve("./lib");
+import path from "path"
+import BundleDeclarationsPlugin from "bundle-declarations-webpack-plugin"
+import { EnvironmentPlugin } from "webpack";
 
-export default <webpack.Configuration>{
-    target: "node",
-    mode: "production",
-    entry: "./src/index.ts",
-    output: {
-        filename: "index.js",
-        path: outDir,
-        publicPath: "/",
-        library: packageName,
-        libraryTarget: "umd",
-        umdNamedDefine: true
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx'],
-        modules: ["node_modules"]
-    },
-    module: {
-        rules: [
-            {
-                test: /\.[tj]sx?$/i,
-                include: resolve("./src"),
-                use: {
-                    loader: "ts-loader",
-                    options: {
-                        onlyCompileBundledFiles: true,
-                        configFile: resolve("./tsconfig.json")
-                    }
-                }
+export default async function (env: Record<string | symbol, string>) {
+    const
+        packageConfig = await import("./package.json"),
+        outDir = path.resolve(__dirname, "lib");
+
+    return {
+        mode: "production",
+        entry: path.resolve(__dirname, "src", "index.ts"),
+        output: {
+            library: {
+                name: packageConfig.name,
+                type: "umd"
+            },
+            path: outDir,
+            filename: "index.js"
+        },
+        externals: {
+            lodash: {
+                commonjs: 'lodash',
+                commonjs2: 'lodash',
+                amd: 'lodash',
+                root: '_',
+            },
+            react: "react",
+            "react-dom": "react-dom",
+            "styled-components": "styled-components"
+        },
+        resolve: {
+            modules: [path.resolve(__dirname, "src"), "node_modules"],
+            extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js", "mjs", "cjs", ".scss", ".module.scss"],
+            alias: {
+
             }
-        ]
-    },
-    plugins: [
-        new CleanWebpackPlugin({
-            dry: false,
-            protectWebpackAssets: true,
-            cleanStaleWebpackAssets: true,
-            cleanOnceBeforeBuildPatterns: [
-                resolve("./lib/**/*.*")
+        },
+        plugins: [
+            new EnvironmentPlugin(),
+            new BundleDeclarationsPlugin({
+                entry: path.resolve(__dirname, "src", "index.ts"),
+                outFile: "index.d.ts",
+                removeRelativeReExport: true
+            })
+        ],
+        module: {
+            rules: [
+                {
+                    test: /(?<!\.(?:stories|test|spec))\.(?:[tj]sx?|[mc]js)$/i,
+                    include: path.join(__dirname, "src"),
+                    use: [
+                        "ts-loader"
+                    ]
+                },
+                {
+                    test: /\.(?:jpe?g|png|gif|svg|eot|ttf|woff2?)$/i,
+                    type: "asset" // file or data url depending on size
+                },
+                {
+                    test: /\.(?:ico|txt|config|html|xml)$/i,
+                    type: "asset/resource" // always file
+                }
             ]
-        }),
-        new CombineDeclarationsWebpackPlugin({
-            name: packageName,
-            main: join(outDir, "index.d.ts"),
-            out: join(outDir, "index.d.ts"),
-            removeSource: true,
-            outputAsModuleFolder: true
-        })
-    ]
-};
+        },
+        optimization: {
+            usedExports: true
+        }
+    };
+}
